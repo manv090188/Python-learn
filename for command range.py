06607922eb15ea70d9976651946d1fc46c2634a8
manv090188 <manv0901@gmail.com>
2025-11-29 08:06:51 +0530

    more

---
+++ for command range.py
@@ -0,0 +1,13 @@
+#to define a huge range, we use range function
+for price in range (10):
+    print(price)
+    break
+
+#range can be defined within limits
+for list in range (8, 15):
+    print(list)
+    break
+
+#also as a 3rd digit can be added as a step
+for items in range (8, 15, 2):
+    print(items)
