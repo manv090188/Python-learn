b7e1b7709aebe751d02a4dc0211a7db3a4af3f5b
manv090188 <manv0901@gmail.com>
2025-11-30 21:27:56 +0530

    lists

---
+++ Nested Loop/F Challenge for Nested loop.py
@@ -0,0 +1,13 @@
+#print F figure using xxxx
+#xxxxx
+#xx
+#xxxxx
+#xx
+#xx
+
+numbers = [6, 2, 6, 2, 2]
+for count in (numbers):
+    output =' '
+    for y in range(count):
+        output += 'x'
+    print(output)
